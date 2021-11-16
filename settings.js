54cd91ccbef1eeea0810819444e2ca9b7f2d4658
niccogarofalo <67937019+niccogarofalo@users.noreply.github.com>
2021-11-16 14:12:30 -0800

    Condensing index file into more accessible files

---
+++ settings.js
@@ -0,0 +1,65 @@
+// IGNORE THESE DECLARATIONS:
+let study_introduction = [];
+function addToIntro();
+let study_conclusion = [];
+function addToConclusion();
+
+// CHANGE OR MODIFY THIS STUFF:
+
+// There is no guarantee that every participant is assigned a unqiue identifier. Keep this number semi-large (10+)
+// to ensure that it is highly unlikely that two participants are assigned the same identifier.
+let lengthOfParticipantID = 12
+
+let nameOfXLSXfile = "slideshows.xlsx"
+
+addToIntro("text",
+    "Welcome to the experiment.<br><br>" +
+    "After completing the experiment, you will be shown a session ID, <b>make sure to record it.</b><br><br>" + 
+    "Please note, you <b>must</b> have a <b>keyboard</b> for this experiment.<br><br>" +
+    "Press the spacebar to begin.",
+    null)
+
+addToIntro("resize",
+    "Click and drag the lower right corner of the box until the box is the same size as a credit card held up to the screen.",
+    null)
+
+addToIntro("text",
+    "Please advance through the slideshow at your own pace.<br><br>" +
+    "Remember to follow the given instructions.<br><br>" +
+    "Press the spacebar to advance.",
+    new Map([["post_trial_gap", 500]]))
+
+addToConclusion("text",
+    "You have completed this section of the study.<br><br>" + 
+    "You are about to see your session ID,<br><br>" + 
+    "<b>You MUST copy it and paste it into the survey which brought you here.</b><br><br>" +
+    "Once you have copied that code, you may close this tab.<br><br>" +
+    "Press the 'Enter' key to continue",
+    new Map([["key_choices", "Enter"]]))
+
+// THESE FUNCTIONS ARE HOW YOU INTERACT WITH THE PROGRAM
+// I LEFT THEM HERE IN CASE YOU ARE CURIOUS HOW THEY WORK
+function isAcceptedDataType(dataType) {
+    let lowercasedatatype = dataType.toLowerCase()
+    if (lowercasedatatype == "resize" ||
+        lowercasedatatype == "text") {
+        return true;
+    }
+    return false;
+}
+
+function addToIntro(dataType, text, additional_options) {
+    if (isAcceptedDataType(dataType)) {
+        study_introduction.push([dataType.toLowerCase(), "<p>"+text+"</p>", additional_options])
+    } else {
+        console.log("Failed to add data type '" + dataType + "' to the introduction.")
+    }
+}
+
+function addToConclusion(dataType, text, additional_options) {
+    if (isAcceptedDataType(dataType)) {
+        study_conclusion.push([dataType.toLowerCase(), "<p>"+text+"</p>", additional_options])
+    } else {
+        console.log("Failed to add data type '" + dataType + "' to the conclusion.")
+    }
+}
---
+++ slideshowparser.js
@@ -0,0 +1,39 @@
+function makeParticipantId(length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+}
+
+// infoData comes in the form [dataType: string, displayText: string, additionalOptions: Map]
+function parseJavaScriptData(infoData) {
+    let dataType = infoData[0];
+    let displayText = infoData[1];
+    let additionalOptions = infoData[2];
+    switch (dataType) {
+        case "resize":
+            return {
+                type: 'resize',
+                item_width: 3 + 3/8,
+                item_height: 2 + 1/8,
+                prompt: displayText,
+                pixels_per_unit: 150
+            }
+        case "text":
+            let tempStorage = {
+                type: "html-keyboard-response",
+                stimulus: displayText,
+                choice: ["spacebar"]
+            }
+            if (additionalOptions.containsKey('key_choices')) {
+                tempStorage.choice = additionalOptions.get('key_choices')
+            }
+            if (additionalOptions.containsKey('post_trial_gap')) {
+                tempStorage.post_trial_gap = additionalOptions.get('post_trial_gap')
+            }
+            return tempStorage;
+    }
+}
